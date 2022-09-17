ee44cb93c8e7578d005ee5bc7acfe46293d8a18e
itsApurba <apurba.apurbaadhikary@gmail.com>
2022-09-17 02:11:18 +0000

    contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Qualification from "./components/qualification/Qualification";
 import Skills from "./components/skills/Skills";
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Qualification />
+        <Contact />
       </main>
     </>
   );
